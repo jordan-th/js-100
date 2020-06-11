679ed79aee212513fd4e6645af03a2f794c5cd9e
“Jordan <jordan.thomas789@gmail.com>
2020-06-11 11:39:40 -0400

    Remove readline-sync from ch3

--- ch5-flow-control/conditional.js
+++ ch5-flow-control/conditional.js
@@ -1,4 +1,4 @@
-let a = prompt('Enter a number');
+/*let a = prompt('Enter a number');
 
 if (a === '3') {
   console.log("a is 3");
@@ -6,4 +6,21 @@ if (a === '3') {
   console.log("a is 4");
 } else {
   console.log("a is neither 3, nor 4");
-}
+} */
+
+//JavaScript can coerce any value to a boolean value, 
+//and that's what it does in conditional contexts like the if statement.
+
+// >
+console.log(`'za' < 'zb' returns ${'za' < 'zb'}`)
+console.log()
+
+
+// ==
+
+
+// && 
+
+
+// ||
+
